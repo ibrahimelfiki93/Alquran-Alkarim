63c0f7ffa7a861c854cb8ad1f2b795899cf1ec63
ibrahimelfiki93 <ibrahimelfiki93@gmail.com>
2026-06-16 15:04:04 +0300

    Edit

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("fetch", () => {});
-=======
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", () => {});
->>>>>>> 3387cd74037309d2e2667d5e6c7c126ed26a3280
